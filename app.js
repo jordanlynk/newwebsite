4263ed1863957575a1cd3ede28b0bdae641dfc7d
Jordan Kidwell <jkid2023@gmail.com>
2020-09-17 17:07:56 -0700

    added in a what and for loop

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 'use strict'
 
+correctPassword = prompt('Enter your password: ');
+while (correctPassword !== password){
+    correctPassword = prompt('Try again.  Enter your password: ');
+}
+var number = "8"
+for(var i = 0; i < 8; i++){
+    var correctNumber = prompt('Enter a number between 1 and 23: ');
+    if (correctNumber === number){
+        alert('You entered the right number!');
+        break;
+    }
+    if (i === 8 && correctNumber != number){
+        alert('You have been locked out for 2 minutes');
+    }
+}
+
+// this will run until the correct number is put in
+   correctNumber = prompt('Enter a number: ');
+   while (correctNumber !== number){
+       correctNumber = prompt('Try again. Enter a number: ');
+   }
+
 function askUserTimeQuestion(){
     var today = new Date();
 var hourNow = today.getHours();
@@ -21,7 +43,7 @@ document.write(greeting);
 
 function askUserNameQuestion(){
     var questionAnswer = prompt(" What is your name?")
-var greeting=("Toe Beans Are The Best Beans ");
+var greeting=("Toe Beans Are The Best Beans, ");
 
 document.write(greeting + questionAnswer);
 }
